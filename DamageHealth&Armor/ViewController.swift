152cf128fd1c94c418f1a4c4f210b685f60b9b9c
Austin <austingae@gmail.com>
2018-08-07 14:38:35 -0700

    Initial Commit

---
+++ DamageHealth&Armor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DamageHealth&Armor
+//
+//  Created by austin_gae on 8/7/18.
+//  Copyright © 2018 Austin Gae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
