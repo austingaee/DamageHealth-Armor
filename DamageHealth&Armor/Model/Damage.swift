f9bf9b802e47db0354bffd895e1c71fbf486703e
Austin <austingae@gmail.com>
2018-08-07 15:41:57 -0700

    Heal and Damage armor Bar

---
+++ DamageHealth&Armor/Model/Damage.swift
@@ -0,0 +1,10 @@
+//
+//  Damage.swift
+//  DamageHealth&Armor
+//
+//  Created by austin_gae on 8/7/18.
+//  Copyright © 2018 Austin Gae. All rights reserved.
+//
+
+import Foundation
+
---
+++ DamageHealth&Armor/Model/DamageHealth.swift
@@ -0,0 +1,13 @@
+//
+//  DamageHealth.swift
+//  DamageHealth&Armor
+//
+//  Created by austin_gae on 8/7/18.
+//  Copyright © 2018 Austin Gae. All rights reserved.
+//
+
+import Foundation
+
+class Damage {
+    
+}
